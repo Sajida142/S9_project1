cb94803a80455c1cc01d082d1fbe2293823e25c1
sajida142 <sajishaik97@gmail.com>
2026-02-24 10:53:41 +0530

    first commit

--- src/Master.jsx
+++ src/Master.jsx
@@ -3,116 +3,7 @@ import React from 'react'
 function Master() {
   return (
     <div>
-      <body>
-      <style>
-        * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: "Poppins", sans-serif;
-    }
-
-body {
-    background: url("img1.jpg") no-repeat center center/cover;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-}
-
-/* Dark overlay behind form */
-body::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.3);
-}
-
-/* GLASSMORPHISM FORM */
-.container {
-    position: relative;
-    width: 400px;
-    padding: 35px;
-    border-radius: 20px;
-
-   
-}
-
-.container h2 {
-    text-align: center;
-    margin-bottom: 20px;
-    color: #fff;
-    font-weight: 600;
-}
-
-/* Input Fields */
-.input-group {
-    margin-bottom: 15px;
-}
-
-.input-group label {
-    font-size: 14px;
-    color: #f5f5f5;
-    margin-bottom: 5px;
-    display: block;
-}
-
-.input-group input,
-.input-group select {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid rgba(255,255,255,0.4);
-    border-radius: 8px;
-    background: rgba(255,255,255,0.25);
-    color: #fff;
-    outline: none;
-    transition: 0.3s;
-}
-
-.input-group input::placeholder {
-    color: #f0f0f0;
-}
-
-.input-group input:focus,
-.input-group select:focus {
-    border-color: #00c3ff;
-    box-shadow: 0 0 8px rgba(0,195,255,0.7);
-}
-
-/* Submit Button */
-.btn {
-    width: 100%;
-    background: rgba(0, 153, 255, 0.8);
-    color: #fff;
-    padding: 12px;
-    border: none;
-    border-radius: 10px;
-    font-size: 16px;
-    cursor: pointer;
-    transition: 0.3s;
-    font-weight: 600;
-}
-
-.btn:hover {
-    background: rgba(0, 153, 255, 1);
-    transform: scale(1.08);
-    box-shadow: 0 8px 20px rgba(0,153,255,0.7);
-}
-
-/* Success Message */
-.success {
-    display: none;
-    text-align: center;
-    margin-top: 15px;
-    color: #00ff88;
-    font-weight: bold;
-}
-
-</style>
+      
     <div class="container">
         <h2>Registration Form</h2>
 <fieldset>
@@ -170,7 +61,6 @@ body::before {
         </form>
         </fieldset>
     </div>
-    </body>
 </div>
 
 )
